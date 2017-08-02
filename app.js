b5501175eefc11bcdf03d980f768743d34ef32f9
Tan Huynh <tanhuynh1008@gmail.com>
2017-08-02 16:27:20 +0700

    change config

--- app.js
+++ app.js
@@ -14,6 +14,13 @@ const userRoute = require('./routes/user');
 mongoose.connect('mongodb://localhost:rental/rental');
 app.use(morgan('combined'));
 app.use(cors());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,PATCH,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
