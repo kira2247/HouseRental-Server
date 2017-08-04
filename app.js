56589ece4a91df4500ff980465af2c4265d53e63
Tan Huynh <tanhuynh1008@gmail.com>
2017-08-04 10:50:09 +0700

    allow cors

--- app.js
+++ app.js
@@ -15,7 +15,14 @@ const userRoute = require('./routes/user');
 //DB Setup
 mongoose.connect('mongodb://tanhuynh1008:Loveu4ever@ds129043.mlab.com:29043/heroku_prg7ghqc');
 app.use(morgan('combined'));
-app.use(cors());
+
+app.use(cors({
+	"origin": "https://thue-nha.herokuapp.com",
+	"methods": "GET,PUT,PATCH,POST,DELETE,OPTIONS",
+	"allowedHeaders": "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json",
+	"credentials": true
+}));
+
 app.disable('etag');
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", 'https://thue-nha.herokuapp.com');
