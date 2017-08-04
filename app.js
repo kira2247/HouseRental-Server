be2a6700764755954f8d01b4d480269d5a43b659
Tan Huynh <tanhuynh1008@gmail.com>
2017-08-04 10:35:03 +0700

    change server

--- app.js
+++ app.js
@@ -16,8 +16,9 @@ const userRoute = require('./routes/user');
 mongoose.connect('mongodb://tanhuynh1008:Loveu4ever@ds129043.mlab.com:29043/heroku_prg7ghqc');
 app.use(morgan('combined'));
 app.use(cors());
+app.disable('etag');
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", 'https://thue-nha.herokuapp.com');
+    res.header("Access-Control-Allow-Origin", '*');
     res.header("Access-Control-Allow-Credentials", true);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,PATCH,POST,DELETE,OPTIONS');
     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
